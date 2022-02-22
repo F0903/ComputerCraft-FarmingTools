37db1efe5fc7caad6701785c3d2205fd78ec882a
Frederik P <frepbh@gmail.com>
2022-02-22 20:14:20 +0100

    Updated the autofarm file.

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,8 +1,8 @@
-local passPeriod = 90
+local passInterval = 60 * 3
 
 while true do
     print("Starting pass...")
     shell.run("Farm")
     print("Pass complete. Sleeping...")
-    sleep(passPeriod)
+    sleep(passInterval)
 end
